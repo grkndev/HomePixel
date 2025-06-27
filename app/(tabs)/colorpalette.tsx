a6befb6f007035a272ca6578d024b5afcabf7308
grkndev <gweepcreative@gmail.com>
2025-06-28 00:01:49 +0300

    colorpalette

--- app/(tabs)/colorpalette.tsx
+++ app/(tabs)/colorpalette.tsx
@@ -1,8 +1,15 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
+import ColorPicker, { HueSlider, Panel1, Swatches } from "reanimated-color-picker";
 export default function ColorPaletteScreen() {
-    return <SafeAreaView className="bg-zinc-950 flex-1">
-        <Text className="text-white">Renk Paleti</Text>
+    return <SafeAreaView className="bg-zinc-950 flex-1 items-center justify-center p-8">
+        <Text className="text-white text-2xl font-bold">Renk Paleti</Text>
+        <View className="flex-1 items-center justify-center w-full">
+            <ColorPicker style={{ gap: 16, width: "100%" }}>
+                <Panel1 />
+                <HueSlider />
+                <Swatches />
+            </ColorPicker>
+        </View>
     </SafeAreaView>;
 }
